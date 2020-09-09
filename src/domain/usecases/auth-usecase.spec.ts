c0fb811459bdd573a9d6db12701c136a06e0eba7
Henrique Couto <henrique.coutu@live.com>
2020-09-08 22:08:12 -0300

    move sut creation to a factory helper

--- src/domain/usecases/auth-usecase.spec.ts
+++ src/domain/usecases/auth-usecase.spec.ts
@@ -1,15 +1,20 @@
 import MissingParamError from '@/utils/errors/missing-param-error'
 import AuthUseCase from './auth-usecase'
 
+const makeSut = () => {
+  const sut = new AuthUseCase()
+  return { sut }
+}
+
 describe('Auth Use Case', () => {
   test('Should throw if no email is provided', () => {
-    const sut = new AuthUseCase()
+    const { sut } = makeSut()
     const promise = sut.auth(null, null)
     expect(promise).rejects.toThrow(new MissingParamError('email'))
   })
 
   test('Should throw if no password is provided', () => {
-    const sut = new AuthUseCase()
+    const { sut } = makeSut()
     const promise = sut.auth('any@email.com', null)
     expect(promise).rejects.toThrow(new MissingParamError('password'))
   })
