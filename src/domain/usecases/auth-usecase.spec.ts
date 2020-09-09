3af54ad63631285ec2ab58a4c9a9d2be85ef6bc7
Henrique Couto <henrique.coutu@live.com>
2020-09-08 21:53:37 -0300

    create custom error MissingParamError

--- src/domain/usecases/auth-usecase.spec.ts
+++ src/domain/usecases/auth-usecase.spec.ts
@@ -1,10 +1,17 @@
+class MissingParamError extends Error {
+  constructor(message) {
+    super(message)
+    this.name = 'MissingParamError'
+  }
+}
+
 class AuthUseCase {
   async auth(email: string, password: string) {
     if (!email) {
-      throw new Error()
+      throw new MissingParamError('email')
     }
     if (!password) {
-      throw new Error()
+      throw new MissingParamError('password')
     }
   }
 }
@@ -13,12 +20,12 @@ describe('Auth Use Case', () => {
   test('Should throw if no email is provided', () => {
     const sut = new AuthUseCase()
     const promise = sut.auth(null, null)
-    expect(promise).rejects.toThrow(new Error())
+    expect(promise).rejects.toThrow(new MissingParamError('email'))
   })
 
   test('Should throw if no password is provided', () => {
     const sut = new AuthUseCase()
     const promise = sut.auth('any@email.com', null)
-    expect(promise).rejects.toThrow(new Error())
+    expect(promise).rejects.toThrow(new MissingParamError('password'))
   })
 })
