26f1905979290ac6ce48ea812cbbe1e8c788b4b3
Henrique Couto <henrique.coutu@live.com>
2020-09-08 21:43:25 -0300

    Should AuthUseCase throws if no password is provided

--- src/domain/usecases/auth-usecase.spec.ts
+++ src/domain/usecases/auth-usecase.spec.ts
@@ -1,6 +1,9 @@
 class AuthUseCase {
-  async auth(email: string) {
+  async auth(email: string, password: string) {
     if (!email) {
+      throw new Error()
+    }
+    if (!password) {
       throw new Error()
     }
   }
@@ -9,7 +12,13 @@ class AuthUseCase {
 describe('Auth Use Case', () => {
   test('Should throw if no email is provided', () => {
     const sut = new AuthUseCase()
-    const promise = sut.auth(null)
+    const promise = sut.auth(null, null)
+    expect(promise).rejects.toThrow(new Error())
+  })
+
+  test('Should throw if no password is provided', () => {
+    const sut = new AuthUseCase()
+    const promise = sut.auth('any@email.com', null)
     expect(promise).rejects.toThrow(new Error())
   })
 })
