63c5182db3446b7be8fd0e5f030ce855b632eb3b
Henrique Couto <henrique.coutu@live.com>
2020-09-08 22:53:47 -0300

    Ensure AuthUseCase calls LoadUserByEmailRepository with correct email

--- src/domain/usecases/auth-usecase.spec.ts
+++ src/domain/usecases/auth-usecase.spec.ts
@@ -1,21 +1,38 @@
 import MissingParamError from '@/utils/errors/missing-param-error'
-import AuthUseCase from './auth-usecase'
+import AuthUseCase, { ILoadUserByEmailRepository } from './auth-usecase'
+
+const makeLoadUserByEmailRepositorySpy = () => {
+  class LoadUserByEmailRepositorySpy implements ILoadUserByEmailRepository {
+    email = ''
+    async load(email): Promise<any> {
+      this.email = email
+    }
+  }
+  return new LoadUserByEmailRepositorySpy()
+}
 
 const makeSut = () => {
-  const sut = new AuthUseCase()
-  return { sut }
+  const loadUserByEmailRepositorySpy = makeLoadUserByEmailRepositorySpy()
+  const sut = new AuthUseCase(loadUserByEmailRepositorySpy)
+  return { sut, loadUserByEmailRepositorySpy }
 }
 
 describe('Auth Use Case', () => {
-  test('Should throw if no email is provided', () => {
+  test('Should throw if no email is provided', async () => {
     const { sut } = makeSut()
     const promise = sut.auth(null, null)
     expect(promise).rejects.toThrow(new MissingParamError('email'))
   })
 
-  test('Should throw if no password is provided', () => {
+  test('Should throw if no password is provided', async () => {
     const { sut } = makeSut()
     const promise = sut.auth('any@email.com', null)
     expect(promise).rejects.toThrow(new MissingParamError('password'))
+  })
+
+  test('Should call LoadUserByEmailRepository with correct email', async () => {
+    const { sut, loadUserByEmailRepositorySpy } = makeSut()
+    await sut.auth('any@email.com', 'any_password')
+    expect(loadUserByEmailRepositorySpy.email).toBe('any@email.com')
   })
 })
--- src/domain/usecases/auth-usecase.ts
+++ src/domain/usecases/auth-usecase.ts
@@ -1,13 +1,33 @@
 import MissingParamError from '@/utils/errors/missing-param-error'
 
-class AuthUseCase {
-  async auth(email: string, password: string) {
+export interface ILoadUserByEmailRepository {
+  email: string
+  load: (email: string) => Promise<any>
+}
+
+export interface IAuthUseCase {
+  loadUserByEmailRepository: ILoadUserByEmailRepository
+  auth: (email: string, password: string) => Promise<string>
+}
+
+class AuthUseCase implements IAuthUseCase {
+  loadUserByEmailRepository
+
+  constructor(loadUserByEmailRepository: ILoadUserByEmailRepository) {
+    this.loadUserByEmailRepository = loadUserByEmailRepository
+  }
+
+  async auth(email, password): Promise<string> {
     if (!email) {
       throw new MissingParamError('email')
     }
     if (!password) {
       throw new MissingParamError('password')
     }
+
+    await this.loadUserByEmailRepository.load(email)
+
+    return ''
   }
 }
 
