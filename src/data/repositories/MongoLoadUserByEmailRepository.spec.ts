c70fe78502a2dc10a8954c2df4dc4d00798a55d8
Henrique Couto <henrique.coutu@live.com>
2020-09-11 10:58:55 -0300

    ensure MongoLoadUserByEmailRepository throws if no email is provided

--- src/data/repositories/MongoLoadUserByEmailRepository.spec.ts
+++ src/data/repositories/MongoLoadUserByEmailRepository.spec.ts
@@ -1,5 +1,6 @@
 import ILoadUserByEmailRepository from '@/domain/definitions/ILoadUserByEmailRepository'
 import User from '@/domain/entities/User'
+import MissingParamError from '@/utils/errors/MissingParamError'
 
 import { Db, MongoClient } from 'mongodb'
 
@@ -8,6 +9,9 @@ let db: Db
 
 class MongoLoadUserByEmailRepository implements ILoadUserByEmailRepository {
   async load(email): Promise<User> {
+    if (!email) {
+      throw new MissingParamError('email')
+    }
     const user = await db.collection('users').findOne({ email })
     if (user) {
       return new User(user)
@@ -34,5 +38,11 @@ describe('MongoLoadUserByEmailRepository', () => {
     const sut = new MongoLoadUserByEmailRepository()
     const user = await sut.load('any@email.com')
     expect(user).toBeNull()
+  })
+
+  test('Should throw if no email is provided', async () => {
+    const sut = new MongoLoadUserByEmailRepository()
+    const promise = sut.load(null)
+    expect(promise).rejects.toThrow(new MissingParamError('email'))
   })
 })
