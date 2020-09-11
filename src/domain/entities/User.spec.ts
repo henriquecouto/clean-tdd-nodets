3fd4f57818f7ec9e477204b149b15aca6ca1cf5f
Henrique Couto <henrique.coutu@live.com>
2020-09-10 21:28:49 -0300

    ensure User instance has correct values

--- src/domain/entities/User.spec.ts
+++ src/domain/entities/User.spec.ts
@@ -5,4 +5,16 @@ describe('User', () => {
     const sut = new User({ email: 'any@email.com', password: 'any_password' })
     expect(sut.id).toBeDefined()
   })
+
+  test('Should user instance has correct values', () => {
+    const params = {
+      email: 'any@email.com',
+      password: 'any_password',
+      id: 'any_id',
+    }
+    const sut = new User(params, params.id)
+    expect(sut.email).toBe(params.email)
+    expect(sut.password).toBe(params.password)
+    expect(sut.id).toBe(params.id)
+  })
 })
