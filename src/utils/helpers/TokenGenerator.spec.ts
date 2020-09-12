3782de22cd1acb6c96de8219e4b3ffe29986f162
Henrique Couto <email@henriquecouto.com.br>
2020-09-12 15:29:05 -0300

    ensure TokenGenerator calls JWT with correct params

--- src/utils/helpers/TokenGenerator.spec.ts
+++ src/utils/helpers/TokenGenerator.spec.ts
@@ -3,7 +3,11 @@ import TokenGenerator from './TokenGenerator'
 
 jest.mock('jsonwebtoken', () => ({
   token: 'any_token',
-  sign() {
+  payload: '',
+  secret: '',
+  sign(payload: string, secret: string) {
+    this.payload = payload
+    this.secret = secret
     return this.token
   },
 }))
@@ -15,5 +19,14 @@ describe('TokenGenerator', () => {
     jwt.token = null
     const token = await sut.generate('any_id')
     expect(token).toBeNull()
+  })
+
+  test('Should call JWT with correct params', async () => {
+    const sut = new TokenGenerator('any_secret')
+    await sut.generate('any_id')
+    // @ts-ignore
+    expect(jwt.payload).toEqual({ id: 'any_id' })
+    // @ts-ignore
+    expect(jwt.secret).toBe('any_secret')
   })
 })
