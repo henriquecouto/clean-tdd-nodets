4c65ecfc264b9dc8a2e46bcbe1461ef87b08439c
Henrique Couto <email@henriquecouto.com.br>
2020-09-14 14:55:39 -0300

    move sut creation to a factory helper

--- src/presentation/routers/LoginRouter.spec.ts
+++ src/presentation/routers/LoginRouter.spec.ts
@@ -1,9 +1,15 @@
+import MissingParamError from '@/utils/errors/MissingParamError'
 import HttpRequest from '../helpers/HttpRequest'
 import LoginRouter from './LoginRouter'
 
+const makeSut = () => {
+  const sut = new LoginRouter()
+  return { sut }
+}
+
 describe('LoginRouter', () => {
   test('Should return 400 if no email is provided', async () => {
-    const sut = new LoginRouter()
+    const { sut } = makeSut()
     const httpRequest = new HttpRequest({
       body: {
         password: 'any_password',
@@ -11,11 +17,11 @@ describe('LoginRouter', () => {
     })
     const httpResponse = await sut.route(httpRequest)
     expect(httpResponse.statusCode).toBe(400)
-    expect(httpResponse.body.error).toBe(new Error('Invalid Email').message)
+    expect(httpResponse.body.error).toBe(new MissingParamError('email').message)
   })
 
   test('Should return 400 if no password is provided', async () => {
-    const sut = new LoginRouter()
+    const { sut } = makeSut()
     const httpRequest = new HttpRequest({
       body: {
         email: 'any@email.com',
@@ -23,6 +29,8 @@ describe('LoginRouter', () => {
     })
     const httpResponse = await sut.route(httpRequest)
     expect(httpResponse.statusCode).toBe(400)
-    expect(httpResponse.body.error).toBe(new Error('Invalid Password').message)
+    expect(httpResponse.body.error).toBe(
+      new MissingParamError('password').message
+    )
   })
 })
